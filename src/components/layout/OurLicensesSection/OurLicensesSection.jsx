2e21f3b6e0f4cf104f5b97757020b438cdb62ca9
Veniamin Polienko <kasaklalita@mail.ru>
2022-11-22 23:20:25 +0300

    Minor changes

---
+++ src/components/layout/OurLicensesSection/OurLicensesSection.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ContentSection from "../ContentSection/ContentSection.jsx";
+import PrimaryTitle from "../../UI/PrimaryTitle/PrimaryTitle.jsx";
+
+const OurLicensesSection = () => {
+  return (
+    <ContentSection title="Наши лицензия">
+      <PrimaryTitle></PrimaryTitle>
+    </ContentSection>
+  );
+};
+
+export default OurLicensesSection;
